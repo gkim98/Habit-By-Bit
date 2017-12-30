04b8274d04270a1be9fede81e4ba70a948954aa8
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2017-12-30 07:01:53 -0600

    Basic calendar functioning

---
+++ Habit By Bit/Controller/CalendarCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCell.swift
+//  Habit By Bit
+//
+//  Created by Grant Kim on 12/30/17.
+//  Copyright © 2017 2.5 Yellow. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+}
---
+++ Habit By Bit/Controller/CalendarViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CalendarViewController.swift
+//  Habit By Bit
+//
+//  Created by Grant Kim on 12/30/17.
+//  Copyright © 2017 2.5 Yellow. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarViewController: UIViewController {
+    let formatter = DateFormatter()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let cell = cell as! CalendarCell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2017 01 01")!
+        let endDate = formatter.date(from: "2017 12 31")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+}
