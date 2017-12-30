b0382f9ab262aa1cd28f848a800dc3b02cdd5976
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2017-12-30 07:47:35 -0600

    Selected date is highlighted

--- Habit By Bit/Controller/CalendarViewController.swift
+++ Habit By Bit/Controller/CalendarViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    
     let formatter = DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupCalendarView()
+    }
+    
+    func setupCalendarView() {
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,17 +33,7 @@ class CalendarViewController: UIViewController {
 
 }
 
-extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
-    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
-        let cell = cell as! CalendarCell
-    }
-    
-    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
-        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
-        cell.dateLabel.text = cellState.text
-        return cell
-    }
-    
+extension CalendarViewController: JTAppleCalendarViewDataSource {
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
         formatter.dateFormat = "yyyy MM dd"
         formatter.timeZone = Calendar.current.timeZone
@@ -46,5 +44,37 @@ extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarVi
         
         let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
         return parameters
+    }
+}
+
+extension CalendarViewController: JTAppleCalendarViewDelegate {
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let cell = cell as! CalendarCell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        cell.dateLabel.text = cellState.text
+        
+        // guards against problems relating to the reuse of cells
+        if cellState.isSelected {
+            cell.dateLabel.textColor = UIColor.red
+        } else {
+            cell.dateLabel.textColor = UIColor.black
+        }
+        
+        return cell
+    }
+    
+    // function for when a cell is selected
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        guard let validCell = cell as? CalendarCell else {return}
+        validCell.dateLabel.textColor = UIColor.red
+        
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        guard let validCell = cell as? CalendarCell else {return}
+        validCell.dateLabel.textColor = UIColor.black
     }
 }
