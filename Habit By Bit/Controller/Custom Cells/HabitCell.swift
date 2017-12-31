8339f0a80e4ff805a8dd42141f1bc6ea6c8f2847
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2017-12-31 08:26:34 -0600

    Add table view for the habits initial view

---
+++ Habit By Bit/Controller/Custom Cells/HabitCell.swift
@@ -0,0 +1,24 @@
+//
+//  HabitCell.swift
+//  Habit By Bit
+//
+//  Created by Grant Kim on 12/31/17.
+//  Copyright © 2017 2.5 Yellow. All rights reserved.
+//
+
+import UIKit
+
+class HabitCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Habit By Bit/Controller/HabitsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  HabitsViewController.swift
+//  Habit By Bit
+//
+//  Created by Grant Kim on 12/31/17.
+//  Copyright © 2017 2.5 Yellow. All rights reserved.
+//
+
+import UIKit
+
+class HabitsViewController: UIViewController {
+
+    @IBOutlet weak var habitsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpTableView()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+//MARK: - table view
+
+extension HabitsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func setUpTableView() {
+        habitsTableView.delegate = self
+        habitsTableView.dataSource = self
+        habitsTableView.register(UINib(nibName: "HabitCell", bundle: nil), forCellReuseIdentifier: "habitCell")
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        habitsTableView.rowHeight = UITableViewAutomaticDimension
+        habitsTableView.estimatedRowHeight = 120.0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "habitCell", for: indexPath) as! HabitCell
+        return cell
+    }
+    
+    
+}
